21773003d6dbd5ebdee9dabc584e253e80931cbf
Matt Lacey <matt@mrlacey.co.uk>
2025-10-16 10:12:57 +0100

    update dependencies and min supported version

--- src/ConstVisualizer.Tests/GlobalSuppressions.cs
+++ src/ConstVisualizer.Tests/GlobalSuppressions.cs
@@ -4,6 +4,4 @@
 
 using System.Diagnostics.CodeAnalysis;
 
-[assembly: SuppressMessage("StyleCop.CSharp.OrderingRules", "SA1200:Using directives should be placed correctly", Justification = "No docs for tests")]
-[assembly: SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "no docs for tests")]
 [assembly: SuppressMessage("Style", "VSTHRD200:Use \"Async\" suffix for async methods", Justification = "Exception valid for tests")]
